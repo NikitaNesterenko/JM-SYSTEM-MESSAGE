655f93fadaedf4a91173a41701faf08a98cbb2de
Vasili Knyazev <basil_jr_king@mail.ru>
2019-09-27 20:45:50 +0800

    Сделал каркас приложения
    Spring Boot + монолит
    model / abstract-service/ service impl / security...
    
    Проект запускается и можно открыть начальную страницу.

---
+++ webapp/src/main/java/JMSystemMessageApplication.java
@@ -0,0 +1,11 @@
+package jm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JMSystemMessageApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(JMSystemMessageApplication.class);
+    }
+}
