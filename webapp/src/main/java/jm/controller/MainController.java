a6a16166b102a3a68322e9fadfcf1f9a534c88d3
Vasili Knyazev <basil_jr_king@mail.ru>
2019-10-06 22:44:37 +0800

    Вьюха воркспейс и кнопка добавления канала

--- webapp/src/main/java/jm/controller/MainController.java
+++ webapp/src/main/java/jm/controller/MainController.java
@@ -5,6 +5,15 @@ import jm.test.TestDataInitializer;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.ModelAndView;
+
+import java.rmi.NoSuchObjectException;
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
 
 @Controller
 public class MainController {
@@ -31,6 +40,33 @@ public class MainController {
     @GetMapping(value = "/")
     public String indexPage() {
         return "homePage";
+    }
+
+    @PostMapping(value = "/workspace/create")
+    public ModelAndView addUser(@RequestParam("name") String name,
+                                @RequestParam("users") String users, @RequestParam("owner") String owner, @RequestParam("isPrivate") String isPrivate) {
+        ModelAndView modelAndView = new ModelAndView();
+
+        String[] userArray = users.split(" ");
+        List<User> userList = new ArrayList<>();
+        for(String userLogin : userArray) {
+            if(userService.getUserByLogin(userLogin) != null) {
+                userList.add(userService.getUserByLogin(userLogin));
+            }
+        }
+        if(userService.getUserByLogin(owner) == null) {
+            System.out.println("Такого юзера не существует");
+            modelAndView.setViewName("redirect:/workspace");
+            return modelAndView;
+        }
+        boolean b = false;
+        if(isPrivate.equals("true")) {
+            b = true;
+        }
+        Channel channel = new Channel(name, userList, userService.getUserByLogin(owner), b, LocalDate.now());
+        modelAndView.setViewName("redirect:/workspace");
+        channelDAO.createChannel(channel);
+        return modelAndView;
     }
 
     @GetMapping(value = "/test")
---
+++ webapp/src/main/resources/static/js/workspacePage.js
@@ -0,0 +1,16 @@
+function myFunction() {
+    var modal = document.getElementById("addChannelModal");
+    var btn = document.getElementById("addChannelBtn");
+    var span = document.getElementsByClassName("addChannelClose")[0];
+    btn.onclick = function() {
+        modal.style.display = "block";
+    }
+    span.onclick = function() {
+        modal.style.display = "none";
+    }
+    window.onclick = function(event) {
+        if (event.target == modal) {
+            modal.style.display = "none";
+        }
+    }
+}
