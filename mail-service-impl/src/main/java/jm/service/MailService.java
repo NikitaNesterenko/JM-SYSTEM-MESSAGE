116ed65de9926c350342a35c46497a3bd49bbaee
wormogig <wormogig@mail.ru>
2019-10-11 15:50:22 +0300

    Sending inviting message has been added

---
+++ mail-service-impl/src/main/java/jm/service/MailService.java
@@ -0,0 +1,6 @@
+package jm.service;
+
+public interface MailService {
+
+    public void sendInviteMessage(String email, String inviteLink);
+}
---
+++ mail-service-impl/src/main/java/jm/service/MailServiceImpl.java
@@ -0,0 +1,23 @@
+package jm.service;
+
+import org.springframework.mail.SimpleMailMessage;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.stereotype.Service;
+
+@Service
+public class MailServiceImpl implements MailService{
+
+    private JavaMailSender emailSender;
+
+    public MailServiceImpl(JavaMailSender emailSender) {
+        this.emailSender = emailSender;
+    }
+
+    public void sendInviteMessage(String email, String inviteLink) {
+        SimpleMailMessage message = new SimpleMailMessage();
+        message.setTo(email);
+        message.setSubject("Invite mail");
+        message.setText("You invite link: " + inviteLink);
+        emailSender.send(message);
+    }
+}
