88133e8743c70e12396263529c694eae3448714a
Sergey PARSHIN <sergey.parshin@verallia.com>
2019-10-15 17:46:11 +0300

    spring_security v1

---
+++ security/src/main/java/jm/SimpleUrlAuthenticationSuccessHandler.java
@@ -0,0 +1,21 @@
+package jm;
+
+import jm.model.Role;
+import jm.model.User;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.web.authentication.AuthenticationSuccessHandler;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+
+public class SimpleUrlAuthenticationSuccessHandler implements AuthenticationSuccessHandler {
+    @Override
+    public void onAuthenticationSuccess(HttpServletRequest httpServletRequest, HttpServletResponse httpServletResponse, Authentication authentication) throws IOException, ServletException {
+
+        httpServletResponse.sendRedirect("/workspacel");
+    }
+
+}
--- webapp/src/main/java/jm/controller/MainController.java
+++ webapp/src/main/java/jm/controller/MainController.java
@@ -5,10 +5,12 @@ import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
 
 @Controller
+@RequestMapping(value={"/","/auth/**"})
 public class MainController {
     private static final Logger logger = LoggerFactory.getLogger(MainController.class);
 
