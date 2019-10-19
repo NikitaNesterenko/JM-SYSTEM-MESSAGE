d676d899873478126a7dd1cbf880040183a51cab
wormogig <wormogig@mail.ru>
2019-10-19 15:28:26 +0300

    Устранил проблемы, связанные с перемещением мэил сервиса в модуль сервисов

---
+++ webapp/src/main/java/jm/controller/MailTestController.java
@@ -0,0 +1,21 @@
+package jm.controller;
+
+import jm.MailService;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class MailTestController {
+
+    private MailService mailService;
+
+    public MailTestController(MailService mailService) {
+        this.mailService = mailService;
+    }
+
+    @GetMapping("/testmail")
+    public String sendTestInvite() {
+        mailService.sendInviteMessage("Алексей", "wormogig@gmail.com","wormogig@mail.ru","TEST-WORKSPACE", "https://localhost:8080/");
+        return "home-page";
+    }
+}
