c37aa8a6fae348166a31f662c06ea8707130d2d7
Doctor167005 <40469915+Doctor167005@users.noreply.github.com>
2019-10-21 14:53:32 +0400

    Заменить все var на let/const, bind заменить на on. Cоздать отдельный файл filter-channel - туда запихнуть все функции, а в главный файл экспортить.

--- webapp/src/main/resources/static/js/searchChannel.js
+++ webapp/src/main/resources/static/js/searchChannel.js
@@ -1,26 +1,9 @@
 import {getAllChannels} from "./ajax/channelRestController.js";
+import {findEl} from "./filter-channel.js"
 
-//функция поиска совпадений вводимых символов
-function findEl(el, array, value) {
-    var coincidence = false;
-    el.empty();//очищаем список совпадений
-    for (var i = 0; i < array.length; i++) {
-        if (array[i].match('^' + value)) {//проверяем каждый елемент на совпадение побуквенно
-            el.children('li').each(function () {//проверяем есть ли совпавшие елементы среди выведенных
-                if (array[i] === $(this).text()) {
-                    coincidence = true;//если есть совпадения то true
-                }
-            });
-            if (coincidence === false) {
-                el.append('<li class="js-filter-address">' + array[i] + '</li>');//если нету совпадений то добавляем уникальное название в список
-            }
-        }
-    }
-}
 
-var filterInput = $('#filter-address'),
-    filterUl = $('.ul-addresses');
-var channelNames = [];
+const filterInput = $('#filter-channel'), filterUl = $('.ul-channel');
+let channelNames = [];
 
 $(document).ready(() => {
     showAllChannels();
@@ -30,23 +13,23 @@ const showAllChannels = () => {
     const channels = getAllChannels();
     $.each(channels, (i, item) => {
         channelNames.push(`${item.name}`);
-})
+    })
 };
 
 
 //проверка при каждом вводе символа
-filterInput.bind('input propertychange', function () {
+filterInput.on('input propertychange', function () {
 
     if ($(this).val() !== '') {
         filterUl.fadeIn(100);
-        // findEl(filterUl, $(this).data('address'), $(this).val());
         findEl(filterUl, channelNames, $(this).val());
     } else {
         filterUl.fadeOut(100);
     }
 });
+
 //при клике на елемент выпадалки присваиваем значение в инпут и ставим триггер на его изменение
-filterUl.on('click', '.js-filter-address', function (e) {
+filterUl.on('click', '.js-filter-channel', function (e) {
     $('#filter-address').val('');
     filterInput.val($(this).text());
     filterInput.trigger('change');
