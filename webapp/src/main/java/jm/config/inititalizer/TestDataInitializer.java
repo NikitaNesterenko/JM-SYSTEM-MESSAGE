91a3e162a2002e3eef41232a4de10c475d6aeeba
nikita <nikitanesterenkos@gmail.com>
2019-10-22 18:41:58 +0300

    fix

--- webapp/src/main/java/jm/config/inititalizer/TestDataInitializer.java
+++ webapp/src/main/java/jm/config/inititalizer/TestDataInitializer.java
@@ -11,6 +11,7 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 
+import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.util.*;
 
