adba003c841332c9cd2194043a3e332fc2d02524
GudvinZ <Gudvinz1234@gmail.com>
2019-10-29 04:03:47 +0300

    Merge branch 'dev' into kafka-test-sending

--- model/src/main/java/jm/model/KafkaMessage.java
+++ model/src/main/java/jm/model/KafkaMessage.java
@@ -1,50 +1,14 @@
 //package jm.model;
 //
+//import lombok.AllArgsConstructor;
+//import lombok.Data;
+//import lombok.NoArgsConstructor;
+//
+//@Data
+//@AllArgsConstructor
+//@NoArgsConstructor
 //public class KafkaMessage {
 //    private Long channelId;
 //    private Long userId;
 //    private String content;
-//
-//
-//    @Override
-//    public String toString() {
-//        return "KafkaMessage{" +
-//                "channelId=" + channelId +
-//                ", userId=" + userId +
-//                ", content='" + content + '\'' +
-//                '}';
-//    }
-//
-//    public KafkaMessage() {
-//    }
-//
-//    public KafkaMessage(Long channelId, Long userId, String content) {
-//        this.channelId = channelId;
-//        this.userId = userId;
-//        this.content = content;
-//    }
-//
-//    public Long getChannelId() {
-//        return channelId;
-//    }
-//
-//    public void setChannelId(Long channelId) {
-//        this.channelId = channelId;
-//    }
-//
-//    public Long getUserId() {
-//        return userId;
-//    }
-//
-//    public void setUserId(Long userId) {
-//        this.userId = userId;
-//    }
-//
-//    public String getContent() {
-//        return content;
-//    }
-//
-//    public void setContent(String content) {
-//        this.content = content;
-//    }
 //}
