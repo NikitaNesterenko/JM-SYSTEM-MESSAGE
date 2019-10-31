54523310c290ca212c5d1364be1b2aa5a73cf6fc
Doctor167005 <40469915+Doctor167005@users.noreply.github.com>
2019-10-31 13:30:07 +0400

    Websocket Chat возвращаемся к fetch

--- webapp/src/main/resources/static/js/input-box.js
+++ webapp/src/main/resources/static/js/input-box.js
@@ -1,47 +1,11 @@
-//TODO не работает fetch возвращаем ajax потом разобратся
 
-// import {UserRestPaginationService, ChannelRestPaginationService, MessageRestPaginationService} from './rest/entities-rest-pagination.js'
-//
-// const user_id = 1;//Захардкоденные переменные
-// const channel_id = 1;//Захардкоденные переменные
-// const user_service = new UserRestPaginationService();
-// const channel_service = new ChannelRestPaginationService();
-// const message_service = new MessageRestPaginationService();
-//
-// class Message {
-//     constructor(channel, user, content, dateCreate) {
-//         this.channel = channel;
-//         this.user = user;
-//         this.content = content;
-//         this.dateCreate = dateCreate;
-//     }
-// }
-//
-// $('#form_message').submit(function () {
-//     const user_promise = user_service.getById(user_id);
-//     const channel_promise = channel_service.getById(channel_id);
-//     Promise.all([user_promise, channel_promise]).then(value => {  //После того как Юзер и Чаннел будут получены, начнется выполнение этого блока
-//         const user = value[0];
-//         const channel = value[1];
-//
-//         const message_input_element = document.getElementById("form_message_input");
-//         const text_message = message_input_element.value;
-//         message_input_element.value = null;
-//         const currentDate = convert_date_to_format_Json(new Date());
-//         const message = new Message(channel, user, text_message, currentDate);
-//
-//         pushMessage(message);
-//         message_service.create(message);
-//     });
-//     return false;
-// });
-//TODO нужные контроллеры
-import {createMessage} from "./ajax/messageRestController.js";
-import {getUser} from "./ajax/userRestController.js";
-import {getChannelById} from "./ajax/channelRestController.js";
+import {UserRestPaginationService, ChannelRestPaginationService, MessageRestPaginationService} from './rest/entities-rest-pagination.js'
 
 const user_id = 1;//Захардкоденные переменные
 const channel_id = 1;//Захардкоденные переменные
+const user_service = new UserRestPaginationService();
+const channel_service = new ChannelRestPaginationService();
+const message_service = new MessageRestPaginationService();
 
 class Message {
     constructor(channel, user, content, dateCreate) {
@@ -53,20 +17,55 @@ class Message {
 }
 
 $('#form_message').submit(function () {
-    const message_input_element = document.getElementById("form_message_input");
-    const text_message = message_input_element.value;
-    message_input_element.value = null;
-    const currentDate = convert_date_to_format_Json(new Date());
+    const user_promise = user_service.getById(user_id);
+    const channel_promise = channel_service.getById(channel_id);
+    Promise.all([user_promise, channel_promise]).then(value => {  //После того как Юзер и Чаннел будут получены, начнется выполнение этого блока
+        const user = value[0];
+        const channel = value[1];
 
-    const user = getUser(user_id);
-    const channel = getChannelById(channel_id);
+        const message_input_element = document.getElementById("form_message_input");
+        const text_message = message_input_element.value;
+        message_input_element.value = null;
+        const currentDate = convert_date_to_format_Json(new Date());
+        const message = new Message(channel, user, text_message, currentDate);
 
-    const message = new Message(channel, user, text_message, currentDate);
-    pushMessage(message);
-    createMessage(message);
-
+        pushMessage(message);
+        message_service.create(message);
+    });
     return false;
 });
+//TODO нужные контроллеры
+// import {createMessage} from "./ajax/messageRestController.js";
+// import {getUser} from "./ajax/userRestController.js";
+// import {getChannelById} from "./ajax/channelRestController.js";
+//
+// const user_id = 1;//Захардкоденные переменные
+// const channel_id = 1;//Захардкоденные переменные
+//
+// class Message {
+//     constructor(channel, user, content, dateCreate) {
+//         this.channel = channel;
+//         this.user = user;
+//         this.content = content;
+//         this.dateCreate = dateCreate;
+//     }
+// }
+//
+// $('#form_message').submit(function () {
+//     const message_input_element = document.getElementById("form_message_input");
+//     const text_message = message_input_element.value;
+//     message_input_element.value = null;
+//     const currentDate = convert_date_to_format_Json(new Date());
+//
+//     const user = getUser(user_id);
+//     const channel = getChannelById(channel_id);
+//
+//     const message = new Message(channel, user, text_message, currentDate);
+//     pushMessage(message);
+//     createMessage(message);
+//
+//     return false;
+// });
 
 
 
--- webapp/src/main/resources/static/js/message-box.js
+++ webapp/src/main/resources/static/js/message-box.js
@@ -1,6 +1,4 @@
-//TODO меняем весь скрипт
 import {MessageRestPaginationService} from './rest/entities-rest-pagination.js'
-import {getAllMessagesByChannelId} from "./ajax/messageRestController.js";
 
 const channel_id = 1;//Захардкоденные переменные
 const message_service = new MessageRestPaginationService();
@@ -26,33 +24,14 @@ window.updateMessages = function updateMessages() {
     const message_box = document.getElementById("all-messages");
     message_box.innerHTML = "";
 
-//TODO почемуто не работаем возвращаем Ajax
 
-//     const messages_promise = message_service.getAllMessagesByChannelId(channel_id);
-//     messages_promise.then(messages => { //После того как Месседжи будут получены, начнется выполнение этого блока
-//
-//         messages.forEach(function (message, i) {
-//             let messages_queue_context_user_container = document.createElement('div');
-//             const time = message.dateCreate.split(' ')[1];
-//             messages_queue_context_user_container.innerHTML = `
-//                     <div class="message_user_container" id="message_${message.id}_user_${message.user.id}_content">
-//                         <div id="message_${message.id}_user_${message.user.id}_content_header">
-//                             <span class="message__sender">${message.user.name}</span>
-//                             <a><span class="timestamp__label">${time}</span></a>
-//                         </div>
-//                         <span>${message.content}</span>
-//                     </div>`;
-//             message_box.append(messages_queue_context_user_container);
-//         });
-//         message_box.scrollTo(0, message_box.scrollHeight);
-//     });
+    const messages_promise = message_service.getAllMessagesByChannelId(channel_id);
+    messages_promise.then(messages => { //После того как Месседжи будут получены, начнется выполнение этого блока
 
-    const messages = getAllMessagesByChannelId(channel_id);
-
-    messages.forEach(function (message, i) {
-        let messages_queue_context_user_container = document.createElement('div');
-        const time = message.dateCreate.split(' ')[1];
-        messages_queue_context_user_container.innerHTML = `
+        messages.forEach(function (message, i) {
+            let messages_queue_context_user_container = document.createElement('div');
+            const time = message.dateCreate.split(' ')[1];
+            messages_queue_context_user_container.innerHTML = `
                     <div class="message_user_container" id="message_${message.id}_user_${message.user.id}_content">
                         <div id="message_${message.id}_user_${message.user.id}_content_header">
                             <span class="message__sender">${message.user.name}</span>
@@ -60,9 +39,10 @@ window.updateMessages = function updateMessages() {
                         </div>
                         <span>${message.content}</span>
                     </div>`;
-        message_box.append(messages_queue_context_user_container);
+            message_box.append(messages_queue_context_user_container);
+        });
+        message_box.scrollTo(0, message_box.scrollHeight);
     });
-    message_box.scrollTo(0, message_box.scrollHeight);
 };
 
 updateMessages();
