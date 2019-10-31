315e765147f71e4d6b8f8f9657ba9abdf7813b1d
Vasili Knyazev <basil_jr_king@mail.ru>
2019-10-31 19:14:46 +0800

    Add temporary workspace page v2
    v1 html and css files in their "temp" directories
    v2 added channels display from db

---
+++ webapp/src/main/resources/static/js/temp/workspace-page-temp.js
@@ -0,0 +1,35 @@
+import {getAllChannels} from "./ajax/channelRestControllerTemp.js";
+
+
+window.addEventListener('load', function () {
+    const modal = document.getElementById("addChannelModal");
+    const btn = document.getElementById("addChannelButton");
+    const span = document.getElementsByClassName("addChannelClose")[0];
+    btn.onclick = function () {
+        modal.style.display = "block";
+    };
+    span.onclick = function () {
+        modal.style.display = "none";
+    };
+    window.onclick = function (event) {
+        if (event.target === modal) {
+            modal.style.display = "none";
+        }
+    }
+});
+
+$(document).ready(() => {
+    showAllChannels();
+});
+
+const showAllChannels = () => {
+    const channels = getAllChannels();
+    $.each(channels, (i, item) => {
+        $('#id-channel_sidebar__channels__list').append(`<div class="p-channel_sidebar__channel">
+                                                    <button class="p-channel_sidebar__name_button">
+                                                        <i class="p-channel_sidebar__channel_icon_prefix">#</i>
+                                                        <span class="p-channel_sidebar__name-3">${item.name}</span>
+                                                    </button>
+                                                  </div>`);
+    })
+};
