a65c78bd1c473cd2f6ba0df15d8531bfd6f5617c
nikita <nikitanesterenkos@gmail.com>
2019-11-02 02:41:40 +0300

    fix

--- model/src/main/java/jm/model/User.java
+++ model/src/main/java/jm/model/User.java
@@ -1,17 +1,14 @@
 package jm.model;
 
 
-import lombok.Data;
 import lombok.AllArgsConstructor;
+import lombok.Data;
 import lombok.NoArgsConstructor;
 
 import javax.persistence.*;
-import java.util.HashSet;
 import java.util.Set;
 
 @Data
-@NoArgsConstructor
-@AllArgsConstructor
 @Entity
 @AllArgsConstructor
 @NoArgsConstructor
