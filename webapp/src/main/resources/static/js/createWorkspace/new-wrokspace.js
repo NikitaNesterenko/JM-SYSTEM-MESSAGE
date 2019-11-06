0d5997d286702397cec6f03fe8daed8eca5bd6f9
Doctor167005 <40469915+Doctor167005@users.noreply.github.com>
2019-11-06 09:21:06 +0400

    Пытаемся всё заставить работать на Fetch 6/11/2019

--- webapp/src/main/resources/static/js/createWorkspace/new-wrokspace.js
+++ webapp/src/main/resources/static/js/createWorkspace/new-wrokspace.js
@@ -1,10 +1,15 @@
-import {sendEmail} from "../ajax/createWorkspaceRestController.js";
+import {CreateWorkspacePaginationService} from './rest/entities-rest-pagination.js'
+
+const  sendEmail_service = new CreateWorkspacePaginationService();
 
 $(document).ready(function() {
    $("#button-email").click(function(){
       let email = $('#target-email').val();
-      sendEmail(email);
-      window.location.href = "/confirmemail";
-      return false;
+      // sendEmail(email);
+      const result = sendEmail_service.sendEmail(email);
+      result.then(email => { //После того как ответ будет получен, начнется выполнение этого блока
+         window.location.href = "/confirmemail";
+         return false;
+      });
    });
 });
