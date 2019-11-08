4d71cf810f89173e89dbae0a3cd950f4ed183398
wormogig <wormogig@mail.ru>
2019-11-08 15:11:09 +0300

    что-то поменял

--- webapp/src/main/resources/static/js/message-box.js
+++ webapp/src/main/resources/static/js/message-box.js
@@ -68,6 +68,8 @@ window.updateMessages = function updateMessages() {
                                                         </div>
                                                     </div>`;
             message_box.append(messages_queue_context_user_container);
+
+            // notify("Notify form message-box", "Bla-bla-bla");
         });
     });
 };
