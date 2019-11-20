99e12ea0ea15d94540a4335a17877bdea23f7067
Vasili Knyazev <basil_jr_king@mail.ru>
2019-11-20 23:03:51 +0800

    Js refactor. Done.

--- webapp/src/main/java/jm/controller/MainController.java
+++ webapp/src/main/java/jm/controller/MainController.java
@@ -25,10 +25,10 @@ public class MainController {
     @Autowired
     WorkspaceService workspaceService;
 
-    /*@GetMapping(value = "/")
+    @GetMapping(value = "/")
     public String indexPage() {
         return "home-page";
-    }*/
+    }
 
     @PostMapping(value = "/workspace/create")
     public ModelAndView addUser(@RequestParam("name") String name, @RequestParam("usersList") String[] usersList,
@@ -39,7 +39,7 @@ public class MainController {
         return modelAndView;
     }
 
-    @GetMapping(value = "/")
+    @GetMapping(value = "/workspace")
     public ModelAndView workspacePage() {
         return new ModelAndView("workspace-page");
     }
