c8d2f342ce2593c71343ddb81a6425fa5b6ad4f9
solon4ak <solon4ak@gmail.com>
2019-11-26 16:10:13 +0300

    fixed import for Lombok @NoArgsConstructor

--- model/src/main/java/jm/model/Role.java
+++ model/src/main/java/jm/model/Role.java
@@ -2,6 +2,7 @@ package jm.model;
 
 import com.fasterxml.jackson.annotation.JsonIgnore;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 import org.springframework.security.core.GrantedAuthority;
 
 import javax.persistence.*;
