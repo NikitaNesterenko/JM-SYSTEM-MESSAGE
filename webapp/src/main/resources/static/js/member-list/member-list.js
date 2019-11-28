85f3e99741b156bc5b702c8e7fa29951343e8c2e
Evgenii Urusov <eurusov@gmail.com>
2019-11-28 21:22:44 +0300

    Done. Maybe some refactoring needed. Also, the style needs some corrections.

---
+++ webapp/src/main/resources/static/js/member-list/member-list.js
@@ -0,0 +1,40 @@
+import {ChannelRestPaginationService} from '../rest/entities-rest-pagination.js'
+
+const channel_service = new ChannelRestPaginationService();
+let memberListExpanded = false;
+
+$(document).ready(() => {
+    const memberListBtn = document.getElementById("memberListBtn");
+    memberListBtn.addEventListener("click", memberListBtnClick)
+});
+
+function memberListBtnClick(ev) {
+    const memberListCaretSymbol = document.getElementById('memberListCaretSymbol');
+    if (!memberListExpanded) {
+        memberListExpanded = true;
+        memberListCaretSymbol.innerText = "▼";
+        const channel_promise = channel_service.getById(sessionStorage.getItem("channelName"));
+        channel_promise.then(function (channel) {
+            const channelUsers = channel.users;
+            console.log("channelUsers:");
+            console.log(channelUsers);
+            const memberListPlaceholder = document.getElementById("memberListPlaceholder");
+            let memberListContent = document.createElement('ul');
+            memberListContent.className = 'p-channel_details__members_list';
+
+            for (let user of channelUsers) {
+                memberListContent.innerHTML += `<li class='p-channel_details__member_list__member'>
+    <button type="button" class="p-channel_details__member_list__member_button"
+            data-user-id="${user.id}"><span>${user.name} ${user.lastName}</span>
+    </button>
+</li>`
+            }
+            memberListPlaceholder.append(memberListContent);
+        })
+    } else {
+        memberListExpanded = false;
+        memberListCaretSymbol.innerText = "►";
+        const memberListPlaceholder = document.getElementById("memberListPlaceholder");
+        memberListPlaceholder.innerHTML = '';
+    }
+}
