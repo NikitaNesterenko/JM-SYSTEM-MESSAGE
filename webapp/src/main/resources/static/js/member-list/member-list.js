9d68bb5b83df634748862e7c959a3739994a743e
Evgenii Urusov <eurusov@gmail.com>
2019-12-05 07:56:40 +0300

    Refactored style.

--- webapp/src/main/resources/static/js/member-list/member-list.js
+++ webapp/src/main/resources/static/js/member-list/member-list.js
@@ -16,8 +16,8 @@ function memberListBtnClick(ev) {
         const channel_promise = channel_service.getById(sessionStorage.getItem("channelName"));
         channel_promise.then(function (channel) {
             const channelUsers = channel.users;
-            console.log("channelUsers:");
-            console.log(channelUsers);
+            // console.log("channelUsers:");
+            // console.log(channelUsers);
             const memberListPlaceholder = document.getElementById("memberListPlaceholder");
             let memberListContent = document.createElement('ul');
             memberListContent.className = 'p-channel_details__members_list';
