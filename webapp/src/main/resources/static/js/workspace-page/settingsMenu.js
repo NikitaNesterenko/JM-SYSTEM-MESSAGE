018d44661b974e54af2f7521d369bf0249315eb9
Qustal <mastkill71rus@gmail.com>
2019-12-11 16:55:32 +0300

    Фронт

--- webapp/src/main/resources/static/js/workspace-page/settingsMenu.js
+++ webapp/src/main/resources/static/js/workspace-page/settingsMenu.js
@@ -1,10 +1,14 @@
+import {ChannelRestPaginationService} from '../rest/entities-rest-pagination.js'
+
+const channel_service = new ChannelRestPaginationService();
 const btn = document.getElementById('settingsMenuButton');
 const modal = document.getElementById('settingsList');
+const modal1 = document.getElementById('additionalOptions');
+const btn1 = document.getElementById('additionalOptionsButton');
 
 window.addEventListener('load', function () {
     // populate select
     let isActive = false;
-
     btn.onclick = function () {
         if (isActive === false) {
             isActive = true;
@@ -15,4 +19,24 @@ window.addEventListener('load', function () {
         }
         return false;
     };
+    btn1.onclick = function () {
+        modal1.style.display = "block";
+        let span = document.createElement("span");
+        const channel_promise = channel_service.getById(sessionStorage.getItem("channelName"));
+        channel_promise.then(response => {
+            // $.each(response, (i, item) => {
+                span.innerHTML =response.name;
+                $('#additionalOptionsHeaderSpan')
+                    .append(span);
+            // });
+            return false;
+        });
+    }
+});
+
+document.addEventListener("mouseup", function (e) {
+    let block = $('settingsList');
+    if (!block.is(e.target)) {
+        modal.style.display = "none";
+    }
 });
