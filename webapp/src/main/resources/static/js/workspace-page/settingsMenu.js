4a9f09d9504bec8d2f6b31381cdf9e06586036e0
Qustal <mastkill71rus@gmail.com>
2019-12-12 16:38:53 +0300

    Фронт

--- webapp/src/main/resources/static/js/workspace-page/settingsMenu.js
+++ webapp/src/main/resources/static/js/workspace-page/settingsMenu.js
@@ -5,6 +5,9 @@ const btn = document.getElementById('settingsMenuButton');
 const modal = document.getElementById('settingsList');
 const modal1 = document.getElementById('additionalOptions');
 const btn1 = document.getElementById('additionalOptionsButton');
+const closeButton = document.getElementsByClassName('additionalOptionsCloseButton');
+const archiveButton = document.getElementById('exampleLink1');
+const archiveModal = document.getElementById('archivingChannels');
 
 window.addEventListener('load', function () {
     // populate select
@@ -19,19 +22,30 @@ window.addEventListener('load', function () {
         }
         return false;
     };
+    closeButton.onclick = function () {
+            modal1.style.display = "none";
+        return false;
+    };
     btn1.onclick = function () {
         modal1.style.display = "block";
         let span = document.createElement("span");
         const channel_promise = channel_service.getById(sessionStorage.getItem("channelName"));
         channel_promise.then(response => {
-            // $.each(response, (i, item) => {
                 span.innerHTML =response.name;
                 $('#additionalOptionsHeaderSpan')
+                    .find('span')
+                    .remove()
+                    .end()
                     .append(span);
             // });
             return false;
         });
-    }
+    };
+    archiveButton.onclick = function () {
+        modal1.style.display = "none";
+        archiveModal.style.display = "block";
+        return false;
+    };
 });
 
 document.addEventListener("mouseup", function (e) {
