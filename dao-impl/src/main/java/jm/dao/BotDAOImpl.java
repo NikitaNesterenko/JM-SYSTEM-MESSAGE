f436c19352674450c204e9ba1ca97216391aaeb8
Алексей <tolochekav@gmail.com>
2019-12-13 23:14:33 +0300

    1. Changed try/catch to Optional

--- dao-impl/src/main/java/jm/dao/BotDAOImpl.java
+++ dao-impl/src/main/java/jm/dao/BotDAOImpl.java
@@ -1,3 +1,4 @@
+
 package jm.dao;
 
 import jm.api.dao.BotDAO;
@@ -10,6 +11,7 @@ import org.springframework.stereotype.Repository;
 
 import javax.persistence.NoResultException;
 import javax.transaction.Transactional;
+import java.util.Optional;
 import java.util.Set;
 
 @Repository
@@ -18,16 +20,13 @@ public class BotDAOImpl extends AbstractDao<Bot> implements BotDAO {
     private static final Logger logger = LoggerFactory.getLogger(BotDAOImpl.class);
 
     @Override
-    public Bot getBotByWorkspaceId(Workspace workspace) {
-        try {
-            return (Bot) entityManager.createNativeQuery("select * from bots where workspace_id=?", Bot.class)
+    public Optional<Bot> getBotByWorkspaceId1(Workspace workspace) {
+        return  Optional.ofNullable((Bot) entityManager.createNativeQuery("select * from bots where workspace_id=?", Bot.class)
                     .setParameter(1, workspace)
-                    .getSingleResult();
-        } catch (NoResultException e) {
-            return null;
-        }
+                    .getSingleResult());
     }
 
     @Override
     public Set<Channel> getChannels(Bot bot) { return bot.getChannels(); }
 }
+
