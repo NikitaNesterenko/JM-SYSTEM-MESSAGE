c73ae4228e70db8c59fe67070ebf2605726a43b6
Danila <dane.witcher@yandex.ru>
2019-12-27 15:49:22 +0300

    Отрефакторил admin-page js и ajax rest controller

--- webapp/src/main/resources/static/js/admin-page/adminPage.js
+++ webapp/src/main/resources/static/js/admin-page/adminPage.js
@@ -1,8 +1,29 @@
 import {DocumentReady} from "./components/documentReady.js"
-import {AddEventListener} from "./components/addEventListener.js"
+import {OnUserEditSubmit} from "./components/onUserEditSubmit.js"
+import {RefreshUserList} from "./components/refreshUserList.js";
+import {ShowFilteredUsers} from "./components/showFilteredUsers.js";
 
-let addEventListener = new AddEventListener();
-window.addEventListener('load', addEventListener.addEventListener() );
+
+window.addEventListener('load', function () {
+
+    const btnMainMenu = document.getElementById("mainMenu");
+    const panelAdmin = document.getElementById("panelAdmin");
+
+    btnMainMenu.onclick = function () {
+        if (panelAdmin.style.display !== "none") {
+            panelAdmin.style.display = "none";
+        } else {
+            panelAdmin.style.display = "block";
+        }
+    };
+    let refreshUserList = new RefreshUserList();
+    refreshUserList.refreshUserList();
+    let showFilteredUsers = new ShowFilteredUsers();
+    let onUserEditSubmit = new OnUserEditSubmit();
+
+    document.getElementById("user_edit_submit").addEventListener("click", onUserEditSubmit.onUserEdiSubmit);
+    document.getElementById("searchValue").addEventListener("keyup", showFilteredUsers.showFilteredUsers);
+});
 
 let documentReady = new DocumentReady();
 $(document).ready(documentReady.documentReady());
