aeb9f092b08f7954036c894878d82277f24f9612
alex <Alexei84-08@mail.ru>
2020-01-13 09:17:53 +0300

    Fix ChannelRestControllerTest

--- webapp/src/test/java/jm/controller/rest/ChannelRestControllerTest.java
+++ webapp/src/test/java/jm/controller/rest/ChannelRestControllerTest.java
@@ -8,9 +8,12 @@ import jm.model.Channel;
 import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
+import org.junit.runner.RunWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.springframework.http.MediaType;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.context.web.WebAppConfiguration;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.MvcResult;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
@@ -23,6 +26,8 @@ import static org.hamcrest.Matchers.*;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
+@WebAppConfiguration
+@RunWith(SpringRunner.class)
 public class ChannelRestControllerTest {
     private static final String URL = "/rest/api/channels/";
     @Mock
@@ -37,7 +42,7 @@ public class ChannelRestControllerTest {
     private MockMvc mockMvc;
 
     @Before
-    public void setUp() throws Exception {
+    public void setUp() {
         mockMvc = MockMvcBuilders.standaloneSetup(channelRestControllerMock).build();
     }
 
@@ -55,57 +60,52 @@ public class ChannelRestControllerTest {
 
     @Test
     public void getChannelById() throws Exception {
-        Long testId1 = 1L;
-        final String getUrl = URL + "{id}";
+        Long testId_1 = 1L;
+        String getUrl = URL + "{id}";
 
         Channel channel = new Channel();
-        channel.setId(testId1);
+        channel.setId(testId_1);
         channel.setName("test");
         channel.setIsPrivate(true);
 
         ChannelDTO channelDTO = new ChannelDTO(channel.getId(), channel.getName(), channel.getIsPrivate());
 
-        when(channelServiceMock.getChannelById(testId1)).thenReturn(channel);
-        when(channelServiceMock.getChannelById(testId1)).thenReturn(channel);
-        mockMvc.perform(get(getUrl, testId1))
+        when(channelServiceMock.getChannelById(testId_1)).thenReturn(channel);
+        when(channelDtoServiceMock.toDto(channel)).thenReturn(channelDTO);
+        mockMvc.perform(get(getUrl, testId_1))
                 .andExpect(status().isOk())
                 .andExpect(content().contentType(MediaType.APPLICATION_JSON_UTF8))
                 .andExpect(jsonPath("$.id", is(1)))
-                .andExpect(jsonPath("$.title", is("Lorem ipsum")));
-//                .andExpect(jsonPath("$.title", is("Foo")));
+                .andExpect(jsonPath("$.name", is("test")))
+                .andExpect(jsonPath("$.isPrivate", is(Boolean.TRUE)));
 
-        verify(channelServiceMock, times(1)).getChannelById(1L);
+        verify(channelServiceMock, times(1)).getChannelById(testId_1);
         verifyNoMoreInteractions(channelServiceMock);
+    }
 
-//        String testId2 = "something_text";
-//        mockMvc.perform(get(getUrl, testId2))
-//                .andExpect(status().isBadRequest());
-//        verify(channelServiceMock, times(1)).getChannelById(any());
+    @Test
+    public void getChannelByIdNotFound() throws Exception {
+        Long testId_1 = 1L;
+        String getUrl = URL + "{id}";
 
-//        Long testId3 = 2L;
-//
-//         channel = new Channel();
-//        channel.setId(testId3);
-//        channel.setName("test");
-//        channel.setIsPrivate(true);
-////
-//         channelDTO = new ChannelDTO(channel.getId(), channel.getName(), channel.getIsPrivate());
+        Channel channel = new Channel();
+        channel.setId(testId_1);
+        channel.setName("test");
+        channel.setIsPrivate(true);
 
-//        when(channelService.getChannelById(testId3)).thenReturn(channel);
-//        when(channelServiceMock.toDto(channel)).thenReturn(channelDTO);
+        ChannelDTO channelDTO = new ChannelDTO(channel.getId(), channel.getName(), channel.getIsPrivate());
 
-//        MvcResult result = mockMvc.perform(get(getUrl, testId1))
+//        when(channelServiceMock.getChannelById(testId_1)).thenReturn(null);
+//        when(channelDtoServiceMock.toDto(channel)).thenReturn(channelDTO);
+        mockMvc.perform(get(getUrl, "111"))
+                .andExpect(status().isBadRequest());
 //                .andExpect(content().contentType(MediaType.APPLICATION_JSON_UTF8))
-//                .andExpect(status().isOk())
-//                .andReturn();
-//        verify(channelServiceMock, times(2)).getChannelById(testId1);
-//
-//        Gson gson = new Gson();
-////        Channel resultChannel = gson.fromJson(result.getResponse().getContentAsString(), Channel.class);
-//        Channel resultChannel = TestUtils.jsonToObject(result.getResponse().getContentAsString(), Channel.class);
-//        Assert.assertEquals(channel.getId(), resultChannel.getId());
-//        Assert.assertEquals(channel.getName(), resultChannel.getName());
+//                .andExpect(jsonPath("$.id", is(1)))
+//                .andExpect(jsonPath("$.name", is("test")))
+//                .andExpect(jsonPath("$.isPrivate", is(Boolean.TRUE)));
 
+        verify(channelServiceMock, times(1)).getChannelById(any());
+        verifyNoMoreInteractions(channelServiceMock);
     }
 
     @Test
