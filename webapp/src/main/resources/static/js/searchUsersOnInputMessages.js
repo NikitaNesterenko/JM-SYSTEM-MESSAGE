00025c9b6a2303efdd40eca0f89d468359f4fbcf
alex <Alexei84-08@mail.ru>
2020-01-25 19:07:47 +0300

    Fix @

--- webapp/src/main/resources/static/js/searchUsersOnInputMessages.js
+++ webapp/src/main/resources/static/js/searchUsersOnInputMessages.js
@@ -1,20 +1,78 @@
 import {UserRestPaginationService} from './rest/entities-rest-pagination.js'
 
+export let users = [];
+
 const user_service = new UserRestPaginationService();
 const modal = document.getElementById('associatedUserList');
+const at = document.getElementById('showAssociatedUsers');
+
+let index;
+let elemkeyDown;
+let positionCursor;
+let sub_user;
+let isSearchUser = false;
+let isAtBuutonClick = false;
+
+let inpMessage = document.getElementById('form_message_input');
+
+at.onclick = function () {
+    if (isAtBuutonClick === true) {
+        modal.style.display = "none";
+        isSearchUser = false;
+        isAtBuutonClick = false;
+    } else {
+        let position = inpMessage.selectionEnd;
+        let text = inpMessage.value;
+        if (text.length < 1) {
+            inpMessage.value = text.slice(0, position) + "@" + text.slice(position);
+        } else {
+            inpMessage.value = text.slice(0, position) + " @" + text.slice(position);
+        }
+        inpMessage.focus();
+        inpMessage.setSelectionRange(position + 2, position + 2);
+
+        isAtBuutonClick = true;
+        pasteUserInText();
+    }
+};
 
 $('#form_message_input').on('input', function () {
-    let text = document.getElementById('form_message_input').value;
-    let sub_user;
+    pasteUserInText();
+});
 
-    if (!text.includes(" ") && text.length >= 1 && text.slice(0,1) === "@") {
+export function pasteUserInText() {
+    let text = inpMessage.value;
+    positionCursor = inpMessage.selectionEnd;
+
+    inpMessage.onkeydown = function handle(e) {
+        elemkeyDown = e.key;
+    };
+
+    searchAt(text);
+
+    if (isSearchUser) {
         modal.style.display = "block";
-        sub_user = text.slice(1);
+        sub_user = text.slice(index, positionCursor);
         showAllUsers(sub_user)
     } else {
         modal.style.display = "none";
     }
-});
+}
+
+function searchAt(text) {
+    if ((text.length <= 1 && text.slice(0, 1) === "@") || (elemkeyDown === "@" && text.slice(positionCursor - 2, positionCursor) === " @") || isSearchUser || isAtBuutonClick) {
+        if (!isSearchUser) {
+            index = positionCursor;
+            isSearchUser = true;
+        }
+    }
+}
+
+function str_replace(str, sub, rep, index) {
+    str = str.split('');
+    str.splice(index, sub.length, rep);
+    return str.join('');
+}
 
 const showAllUsers = (text) => {
     const allUsers = user_service.getAll();
@@ -32,7 +90,7 @@ const showAllUsers = (text) => {
                     );
             });
         });
-    }else{
+    } else {
         allUsers.then(response => {           //После того как Юзеры будут получены, начнется выполнение этого блока
             $.each(response, (i, item) => {
                 if ((item.name).includes(text)) {
@@ -47,23 +105,31 @@ const showAllUsers = (text) => {
 };
 
 $('#associatedUserListSelect').on('change', function () {
-    let str = "";
-    // For multiple choice
-    $("select option:selected").each(function () {
+    let result;
+    $("#associatedUserListForm select option:selected").each(function () {
         users.push($(this).val());
         let text = $(this).text();
         if (text !== ' --') {
-            str += "@" + text + " ";
+            let rep = "@" + text + " ";
+            let sub = "@" + sub_user;
+            result = str_replace(inpMessage.value, sub, rep, index - 1);
+            isSearchUser = false;
+            isAtBuutonClick = false;
         }
     });
 
-    $('#form_message_input').val(str);
+    $('#form_message_input').val(result);
     document.getElementById('associatedUserList').style.display = "none";
+    $("#form_message_input").focus();
 });
 
-document.addEventListener("mouseup", function (e){
+document.addEventListener("mouseup", function (e) {
     let block = $('associatedUserList');
-    if (!block.is(e.target)){
+    let buttonAt = $('#showAssociatedUsers font');
+
+    if (!block.is(e.target) && !buttonAt.is(e.target)) {
         modal.style.display = "none";
+        isSearchUser = false;
+        isAtBuutonClick = false;
     }
 });
