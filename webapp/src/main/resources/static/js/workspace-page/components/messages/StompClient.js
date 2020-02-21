d4d56317b5b964a92e8fd36000d3e317e47a60d7
zuigetsu <zuigetsu@gmail.com>
2020-02-21 19:09:33 +0300

    loose }

--- webapp/src/main/resources/static/js/workspace-page/components/messages/StompClient.js
+++ webapp/src/main/resources/static/js/workspace-page/components/messages/StompClient.js
@@ -306,6 +306,7 @@ export class StompClient {
             console.log(channel.body);
             document.querySelector("#topic_string").textContent = chn.topic;
         });
+    }
 
     sendSlackBotCommand(message) {
         let entity = {
