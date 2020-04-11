d78130e342364c06679050b8ec0eb9bebb390dd5
pkhilenko <pkhilenko@yandex.ru>
2020-04-11 17:53:36 +0600

    добавил анимацию на завершение прелоадера

--- webapp/src/main/resources/static/js/preloader.js
+++ webapp/src/main/resources/static/js/preloader.js
@@ -1,4 +1,8 @@
 window.onload = function() {
     let preloader = document.getElementById('preloader');
-    preloader.style.display = 'none';
-}
+    preloader.style.opacity = '0';
+    setTimeout(function () {
+        preloader.style.display = 'none';
+    }, 300)
+
+};
